8a01cc7b5675930c43201bd9181c9b7af167e07a
Patryk Karwowski <vb.karwowski@gmail.com>
2018-09-16 20:52:42 +0100

    test adding number to array by promiss

--- test/testIndex.js
+++ test/testIndex.js
@@ -8,11 +8,29 @@ describe("Init all", () => {
 })
 
 describe("Test function in myApp", () => {
-  const summary = myApp.summary;
-  it("expect addNum(2,2) to return 4", () => {
-    expect(summary(1,2)).to.equal(3);
-  });
-  it("expect summarize numbers 1,3,4,100 and return 108", () => {
-    expect(summary(1,3,4,100)).to.equal(108);
+  describe("summarize method", () => {
+    const summary = myApp.summary;
+    it("init summary: ", () => {
+      expect(summary).to.be.ok;
+    })
+    it("expect summarize 2,2 and return 4", () => {
+      expect(summary(1,2)).to.equal(3);
+    });
+    it("expect summarize numbers 1,3,4,100 and return 108", () => {
+      expect(summary(1,3,4,100)).to.equal(108);
+    })
+  })
+
+  describe("addNumToArray method: ", () => {
+    const addNumToArray = myApp.addNumToArray;
+    const checkArray = myApp.checkArray;
+    it("expect add '1' and '10' and '100' to array and return array", () => {
+      addNumToArray(1).then( ()=>{
+        return addNumToArray(10);
+      }).then( () => {
+        return addNumToArray(100);
+      }); 
+      expect(checkArray()).to.equal(111);
+    })
   })
 })
